dcfbdaebbbd8f3124566805925b004f8989a11fe
ntl22 <nxtung21@outlook.com>
2024-01-01 16:06:10 +0700

    update the draft cascade

--- src/draft/draft.controller.ts
+++ src/draft/draft.controller.ts
@@ -1,7 +1,59 @@
-import { Controller } from '@nestjs/common';
+import {
+  HttpException,
+  Controller,
+  Get,
+  Param,
+  HttpStatus,
+  Post,
+  Body,
+} from '@nestjs/common';
 import { DraftService } from './draft.service';
+import { QuizDto } from './dtos/quiz.dto';
 
 @Controller('draft')
 export class DraftController {
   constructor(private readonly draftService: DraftService) {}
+
+  @Get(':quiz_id')
+  async getQuiz(@Param('quiz_id') quiz_id: string): Promise<QuizDto> {
+    const quiz = await this.draftService.getDraft(quiz_id);
+
+    if (quiz === null) {
+      throw new HttpException('Quiz not found', HttpStatus.BAD_REQUEST);
+    }
+
+    return {
+      quiz_id: quiz.quiz_id,
+      auth_id: quiz.auth_id,
+      title: quiz.draft.title,
+      description: quiz.draft.description,
+      num_play_times: quiz.num_play_times,
+      is_public: quiz.draft.is_public,
+      created_at: quiz.created_at.toISOString(),
+      questions: quiz.draft.questions.map((question) => ({
+        index: question.index,
+        question: question.question,
+        time_limit: question.time,
+        allow_powerups: question.allow_powerups,
+        answers: question.answers.map((answer) => ({
+          index: answer.index,
+          answer: answer.answer,
+          is_correct: answer.is_correct,
+        })),
+      })),
+    };
+  }
+
+  @Post()
+  async updateQuiz(@Body() dto: QuizDto): Promise<{
+    quiz_id: string;
+  }> {
+    if (dto.quiz_id === undefined) {
+      const quiz = await this.draftService.createDraft(dto);
+      return { quiz_id: quiz.quiz_id };
+    }
+
+    const quiz = await this.draftService.updateDraft(dto);
+    return { quiz_id: quiz.quiz_id };
+  }
 }
--- src/draft/draft.service.ts
+++ src/draft/draft.service.ts
@@ -1,7 +1,120 @@
 import { Injectable } from '@nestjs/common';
 import { EntityManager } from '@mikro-orm/postgresql';
+import { Quiz } from 'src/entities/quiz.entity';
+import { QuizDto } from './dtos/quiz.dto';
+import { Version } from 'src/entities/version.entity';
+import { Question } from 'src/entities/question.entity';
+import { Answer } from 'src/entities/answer.entity';
 
 @Injectable()
 export class DraftService {
   constructor(private readonly em: EntityManager) {}
+
+  async getDraft(quiz_id: string) {
+    return await this.em.findOne(
+      Quiz,
+      {
+        quiz_id,
+        $not: { draft: null },
+      },
+      {
+        populate: ['draft', 'draft.questions', 'draft.questions.answers'],
+      },
+    );
+  }
+
+  async createDraft(dto: QuizDto) {
+    const quiz = new Quiz();
+
+    if (dto.quiz_id !== undefined) {
+      quiz.quiz_id = dto.quiz_id;
+    }
+
+    quiz.auth_id = dto.auth_id;
+    quiz.num_play_times = dto.num_play_times;
+
+    const draft = new Version();
+    draft.title = dto.title;
+    draft.description = dto.description;
+    draft.is_public = dto.is_public;
+    draft.quiz = quiz;
+    quiz.draft = draft;
+
+    draft.questions.set(
+      dto.questions.map((question) => {
+        const q = new Question();
+
+        q.index = question.index;
+        q.question = question.question;
+        q.time = question.time_limit;
+        q.allow_powerups = question.allow_powerups;
+        q.version = draft;
+
+        q.answers.set(
+          question.answers.map((answer) => {
+            const a = new Answer();
+
+            a.index = answer.index;
+            a.answer = answer.answer;
+            a.is_correct = answer.is_correct;
+            a.question = q;
+
+            return a;
+          }),
+        );
+
+        return q;
+      }),
+    );
+
+    await this.em.persistAndFlush(quiz);
+    return quiz;
+  }
+
+  async updateDraft(dto: QuizDto) {
+    const quiz = await this.em.findOneOrFail(
+      Quiz,
+      { quiz_id: dto.quiz_id },
+      { populate: true },
+    );
+    quiz.draft = quiz.draft ?? new Version();
+
+    const draft = quiz.draft;
+    draft.title = dto.title;
+    draft.description = dto.description;
+    draft.is_public = dto.is_public;
+    draft.quiz = quiz;
+
+    await this.em.nativeDelete(Question, { version: draft });
+
+    draft.questions.set(
+      dto.questions.map((question) => {
+        const q = new Question();
+
+        q.index = question.index;
+        q.question = question.question;
+        q.time = question.time_limit;
+        q.allow_powerups = question.allow_powerups;
+        q.version = draft;
+
+        q.answers.set(
+          question.answers.map((answer) => {
+            const a = new Answer();
+
+            a.index = answer.index;
+            a.answer = answer.answer;
+            a.is_correct = answer.is_correct;
+            a.question = q;
+
+            return a;
+          }),
+        );
+
+        return q;
+      }),
+    );
+
+    await this.em.persistAndFlush(quiz);
+    return quiz;
+  }
 }
