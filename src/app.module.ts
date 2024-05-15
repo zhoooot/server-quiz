226bfd947fb25588961a50332598112d645d6152
Phạm Võ Quỳnh Như <phamvoquynhnhu24@gmail.com>
2024-05-15 23:54:31 +0700

    idk, update quiz?

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,12 @@
 import { Module } from '@nestjs/common';
 import { config } from './mikro-orm.config';
 import { MikroOrmModule } from '@mikro-orm/nestjs';
-import { DraftModule } from './draft/draft.module';
 import { QuizModule } from './quiz/quiz.module';
 import { RabbitmqModule } from './rabbitmq/rabbitmq.module';
 
 @Module({
   imports: [
     MikroOrmModule.forRoot(config),
-    DraftModule,
     QuizModule,
     RabbitmqModule,
   ],
--- src/draft/draft.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { DraftService } from './draft.service';
-import { DraftController } from './draft.controller';
-
-@Module({
-  providers: [DraftService],
-  controllers: [DraftController],
-})
-export class DraftModule {}
